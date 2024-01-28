16df3df651f99521b3bb1fbc3b7628dae140ea44
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-28 23:09:33 +0100

    custom components

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// import Greet from "./components/Greet";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.container}>Stylesheet API</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //occupy entire available space
+    backgroundColor: "plum",
+    padding: 60,
   },
+  title: {},
 });
